6fd303ef471df14c0931e46a31802b9cea28c302
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-01 18:14:56 -0400

    added team page

---
+++ src/pages/the-team.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TheTeamPage = () => {
+    return (
+        <Layout>
+            <SEO title="The Team" />
+            <h1>Meet the Team</h1>
+        </Layout>
+    )
+}
