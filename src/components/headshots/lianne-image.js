ec50307d3f33ab43ecf7f1a1b986bfa7cba22e11
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-01 20:40:50 -0400

    completed meet the team page

---
+++ src/components/headshots/lianne-image.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const LianneImage = () => (
+    <StaticQuery
+        query={graphql`
+            query {
+                file(relativePath: { eq: "headshots/lianne.jpg" }) {
+                    childImageSharp {
+                        # Specify a fluid image and fragment
+                        # The default maxWidth is 800 pixels
+                        fluid {
+                            ...GatsbyImageSharpFluid_tracedSVG
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <Img
+                fluid={data.file.childImageSharp.fluid}
+                className="person-image first"
+            />
+        )}
+    />
+)
+export default LianneImage
---
+++ src/components/headshots/lucas-image.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const LucasImage = () => (
+    <StaticQuery
+        query={graphql`
+            query {
+                file(relativePath: { eq: "headshots/lucas.jpg" }) {
+                    childImageSharp {
+                        # Specify a fluid image and fragment
+                        # The default maxWidth is 800 pixels
+                        fluid {
+                            ...GatsbyImageSharpFluid_tracedSVG
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <Img
+                fluid={data.file.childImageSharp.fluid}
+                className="person-image"
+            />
+        )}
+    />
+)
+export default LucasImage
