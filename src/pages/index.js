54d5704688770b993e6d208a13552d10481c06db
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-03 13:39:20 -0400

    added coaster language

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,9 +44,9 @@ const IndexPage = () => {
                         to wonderful people who've donated to one of these BLM
                         or LGBTQ+ <Link to="/charities/">charities</Link>! These
                         octopi are hand-made with love by our talented team. We
-                        also offer octopus themed bracelets and scrunchies for
-                        $5! Meet the octopi and other products at our{" "}
-                        <Link to="/gallery/">gallery</Link>.
+                        also offer octopus themed bracelets, scrunchies, and
+                        coasters for $5! Meet the octopi and other products at
+                        our <Link to="/gallery/">gallery</Link>.
                     </p>
                     <p>
                         If you'd like to help in other ways, check out our{" "}
@@ -82,10 +82,9 @@ const IndexPage = () => {
                 <p className="step-instruction">
                     <strong>Let us know!</strong> Send us a picture of your
                     donation confirmation and let us know whether you want a
-                    crochet octopus, bracelet, or a scrunchie, along with the
-                    color you want it in. The bracelets come with octopi charms
-                    and can be pride themed. You can do this through our
-                    Instagram{" "}
+                    crochet octopus, bracelet, scrunchie, or a coaster, along
+                    with the color you want it in. All of those are octopi
+                    themed! You can do this through our Instagram{" "}
                     <a href="https://www.instagram.com/the_octopus_project">
                         @the_octopus_project
                     </a>
