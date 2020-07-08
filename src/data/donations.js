ecfd1172c101a51bd6804def987991069236c1ba
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-08 09:30:32 -0400

    added raffle donation

--- src/data/donations.js
+++ src/data/donations.js
@@ -1014,8 +1014,9 @@ const donationData = [
         org: "Campaign Zero",
         id: 77,
         date: "2020-07-08",
-        order: ["octopus"],
+        order: ["octopus", "raffle"],
         octopusColor: ["teal"],
+        raffleEntry: 5,
         sent: false,
     },
 ]
