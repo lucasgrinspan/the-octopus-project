4a18cef1e1ba30516cee505f7e76ca237fa2b90a
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-10 00:46:03 -0400

    removed images and edited 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>This page doesn&#39;t exist... the sadness.</p>
+        <p>
+            Go back <Link to="/">home</Link>.
+        </p>
+    </Layout>
 )
 
 export default NotFoundPage
