23c2c06997ddbb0a30d6846309bedeb3b12eaf17
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-10 00:46:38 -0400

    404 page title

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import { Link } from "gatsby"
 const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
+        <h1>Oops...</h1>
         <p>This page doesn&#39;t exist... the sadness.</p>
         <p>
             Go back <Link to="/">home</Link>.
