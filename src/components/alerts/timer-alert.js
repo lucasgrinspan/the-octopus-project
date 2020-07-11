4ae995fb72ad9d50613d0a8ad4498eddb10c9777
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-11 14:03:50 -0400

    added rachel text

--- src/components/alerts/timer-alert.js
+++ src/components/alerts/timer-alert.js
@@ -7,7 +7,7 @@ const TimerAlert = () => {
         <div className="alert alert-error">
             <IoIosHourglass className="alert-icon" />
             <div className="alert-content">
-                <p className="alert-title">Congrats to the winner!</p>
+                <p className="alert-title">Congrats to Rachel!</p>
                 <p className="alert-text">
                     This raffle has ended! Stay posted for any future raffles!
                 </p>
