47fc8e832b5e1cb42aa57184a6bc277fd93bf069
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-07-18 19:32:48 -0400

    changed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,18 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Link } from "gatsby"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
-        <h1>Oops...</h1>
-        <p>This page doesn&#39;t exist... the sadness.</p>
+        <h1>This page doesn't exist!</h1>
+        <p>The sadness...</p>
         <p>
-            Go back <Link to="/">home</Link>.
+            <Link to="/">Take me home</Link>
         </p>
     </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
