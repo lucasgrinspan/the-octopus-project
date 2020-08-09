b58c0829640066fafe30cdf177c78c16c63eedd8
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-08-09 15:15:48 -0400

    disabled form

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,36 +1,34 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import { IoIosImage } from "react-icons/io"
+import { IoIosImage } from "react-icons/io";
 
-import "./contact-form.css"
+import "./contact-form.css";
 
 const openFilePicker = () => {
-    let imagePicker = document.getElementById("image-picker-input")
+    let imagePicker = document.getElementById("image-picker-input");
     if (imagePicker) {
-        imagePicker.click()
+        imagePicker.click();
     }
-}
+};
 
 const ContactForm = () => {
-    const [file, setFile] = useState(null)
+    const [file, setFile] = useState(null);
 
     const changeImage = evt => {
-        setFile(URL.createObjectURL(evt.target.files[0]))
-    }
+        setFile(URL.createObjectURL(evt.target.files[0]));
+    };
 
     let imageIcon = file ? (
         <img src={file} id="img-thumbnail" alt="Your upload" />
     ) : (
         <IoIosImage style={{ marginLeft: 15 }} />
-    )
+    );
 
+    const onSubmit = evt => {
+        evt.preventDefault();
+    };
     return (
-        <form
-            action="https://getform.io/f/f0218d9a-515e-4ae5-8765-faf3de588498"
-            encType="multipart/form-data"
-            id="contact-form"
-            method="POST"
-        >
+        <form id="contact-form" onSubmit={onSubmit}>
             <label>
                 Name (or a fake name!)
                 <input type="text" name="name" placeholder="Octo Friend" />
@@ -69,7 +67,7 @@ const ContactForm = () => {
                 Send
             </button>
         </form>
-    )
-}
+    );
+};
 
-export default ContactForm
+export default ContactForm;
